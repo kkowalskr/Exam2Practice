f859403a075d6fb8fcf021464a95ee8dee47456f
kowalskr <kowalskr@rose-hulman.edu>
2018-01-27 21:46:37 -0500

    finished m1

--- src/practice_problem1.py
+++ src/practice_problem1.py
@@ -46,8 +46,8 @@ def main():
     run_test_double_then_shrink()
     run_test_reset()
     run_test_steal()
-#     run_test_get_history()
-#     run_test_combined_box()
+    run_test_get_history()
+    run_test_combined_box()
 
 
 ########################################################################
@@ -109,6 +109,7 @@ class Box(object):
             self.contents = ''
         self.orig_contents = self.contents
         self.orig_volume = self.volume
+        self.history = []
 
     def append_string(self, additional_contents):
         """
@@ -390,6 +391,7 @@ class Box(object):
         #    TIME ESTIMATE:   5 minutes.
         # --------------------------------------------------------------
 
+        self.history.append(self.contents)
         self.contents = self.orig_contents
         self.volume = self.orig_volume
 
@@ -459,7 +461,7 @@ class Box(object):
           #   h is now ['GoodGo', 'GoodBye']
         """
         # --------------------------------------------------------------
-        # TODO: 9. Implement and test this function.
+        # DONE: 9. Implement and test this function.
         #     The testing code is already written for you (above).
         # --------------------------------------------------------------
         # --------------------------------------------------------------
@@ -467,6 +469,8 @@ class Box(object):
         #    DIFFICULTY:      6
         #    TIME ESTIMATE:   5 minutes.
         # --------------------------------------------------------------
+
+        return self.history
 
     def combined_box(self, other_box):
         """
@@ -494,6 +498,8 @@ class Box(object):
         #    TIME ESTIMATE:   5 minutes.
         # --------------------------------------------------------------
 
+        return Box(self.contents + other_box.contents, self.volume +
+                      other_box.volume)
 
 ########################################################################
 # The TEST functions for the  Box  class begin here.
